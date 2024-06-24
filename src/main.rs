0d7fccb49ed85592191764aac9805368b8ad65a9
yohaneYuri <164520341+yohaneYuri@users.noreply.github.com>
2024-06-24 15:05:01 +0800

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#![windows_subsystem = "windows"]
 use std::time;
 
 use regex::Regex;
@@ -5,55 +6,58 @@ use reqwest::Client;
 use tokio::fs;
 use winrt_notification::{Sound, Toast};
 
+fn send_notification(title: &str, text1: &str, text2: &str) {
+    Toast::new(Toast::POWERSHELL_APP_ID)
+                    .title(title)
+                    .text1(text1)
+                    .text2(text2)
+                    .sound(Some(Sound::SMS))
+                    .duration(winrt_notification::Duration::Short)
+                    .show()
+                    .unwrap();
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let start = time::Instant::now();
-    let client = Client::new();
+    let prog_start = time::Instant::now();
+    let client = Client::builder()
+        .timeout(time::Duration::from_secs(3))
+        .build()?;
     let url = fs::read_to_string("./assets/url.txt").await?;
     let regex = Regex::new(r#""result":\s*([01])"#).unwrap();
     let mut count: i64 = 0;
-    
+
     loop {
-        let content = client.get(url.clone())
+        count += 1;
+        let response = client.get(url.clone())
             .send()
-            .await?
-            .text()
-            .await?;
+            .await;
+        if response.is_err() {
+            continue;
+        }
+        let content = response.unwrap().text().await?;
         let result = regex.captures(&content)
-            .map(|cap| cap.get(1))
-            .flatten()
+            .and_then(|cap| cap.get(1))
             .unwrap()
             .as_str()
             .parse::<i64>()
             .unwrap();
 
-        match result {
-            1 => {
-                let end = time::Instant::now();
-                Toast::new(Toast::POWERSHELL_APP_ID)
-                    .title("通知")
-                    .text1("校园网连接成功")
-                    .text2(format!("用时：{:?}", end - start).as_str())
-                    .sound(Some(Sound::SMS))
-                    .duration(winrt_notification::Duration::Short)
-                    .show()
-                    .unwrap();
+        if result == 1 {
+            let prog_end = time::Instant::now();
+                send_notification(
+                    "通知",
+                    "校园网连接成功",
+                    format!("尝试{}次，用时：{:?}", count, prog_end - prog_start).as_str()
+                );
                 break;
-            },
-            0 | _  => {
-                if count == 2 {
-                    Toast::new(Toast::POWERSHELL_APP_ID)
-                        .title("通知")
-                        .text1("校园网连接失败")
-                        .sound(Some(Sound::SMS))
-                        .duration(winrt_notification::Duration::Short)
-                        .show()
-                        .unwrap();
-                }
-
-                count += 1;
-            }
-        };
+        } else if result == 0 {
+            send_notification(
+                "通知",
+                "校园网连接失败",
+                ""
+            );
+        }
     };
     Ok(())
 }
